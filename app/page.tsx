442eeb3ee838ec50c3fa7429572277c037399b41
HamSangEok <hamsoo159@naver.com>
2025-12-22 16:46:16 +0900

    about

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Main from "@/component/Main";
 
 export default function Home() {
+  console.log("애는되나?")
   return (
     <div className="w-full h-screen">
       <Main />
--- component/Main.tsx
+++ component/Main.tsx
@@ -8,9 +8,7 @@ export default function Main() {
 
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center gap-4">
-      <div className="text-2xl font-bold animate-fade-in">
-        나는 결말을 알고 있다.
-      </div>
+      <div className="text-2xl font-bold animate-fade-in">나는 결말을 알고 있다.</div>
       <div className="flex flex-col gap-4 animate-fade-in">
         <Button onClick={() => router.push("/game")} content="Get Started..." />
       </div>
